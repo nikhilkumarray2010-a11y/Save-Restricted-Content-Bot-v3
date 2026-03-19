358fc06e806e410f041ecf570a77dfef1038ed7d
nikhilkumarray2010-a11y <nikhilkumarray2010@gmail.com>
2026-03-19 22:08:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,12 +41,12 @@ YT_COOKIES   = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 
 # ─── USAGE LIMITS ───────────────────────────────────────────────────────────────
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT  = int(os.getenv("PREMIUM_LIMIT", "500"))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "100"))
+PREMIUM_LIMIT  = int(os.getenv("PREMIUM_LIMIT", "500000000"))
 
 # ─── UI / LINKS ─────────────────────────────────────────────────────────────────
-JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro")
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/NIKHIL_BHAII_BOTS")
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/NIKHIL_BHAII_CONTACT_BOT")
 
 # ════════════════════════════════════════════════════════════════════════════════
 # ░ PREMIUM PLANS CONFIGURATION
